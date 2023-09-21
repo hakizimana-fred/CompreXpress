c6563efcaffa51c21c0185b2d648d2b1b8f5940d
Hakizimana Fred <hakifred20@gmail.com>
2023-09-21 11:47:32 +0300

    faile path validity

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("well done");
+import { existsSync } from "fs";
+
+class ReadFile {
+  uploadFile(filePath: string): any {
+    if (this.isValidPath(filePath)) {
+      console.log("file path is valid");
+    } else {
+      console.log("this is file path is invalid");
+    }
+  }
+
+  isValidPath(filePath: string) {
+    return existsSync(filePath);
+  }
+}
+
+const main = () => {
+  const file = new ReadFile();
+  file.uploadFile("");
+};
+
+void main();
